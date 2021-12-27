3a187fef18845971aa4056b924584f4bdf5b4401
Keven Leone <keven.santos.sz@gmail.com>
2021-12-27 10:44:23 -0300

    Use anonymous func

--- scripts/backup-github.js
+++ scripts/backup-github.js
@@ -14,22 +14,24 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-let username = await question('What is your GitHub username? ')
-let token = await question('Do you have GitHub token in env? ', {
-  choices: Object.keys(process.env)
-})
-
-let headers = {}
-if (process.env[token]) {
-  headers = {
-    Authorization: `token ${process.env[token]}`
+(async () => {
+  let username = await question('What is your GitHub username? ')
+  let token = await question('Do you have GitHub token in env? ', {
+    choices: Object.keys(process.env)
+  })
+  
+  let headers = {}
+  if (process.env[token]) {
+    headers = {
+      Authorization: `token ${process.env[token]}`
+    }
   }
-}
-let res = await fetch(`https://api.github.com/users/${username}/repos`, {headers})
-let data = await res.json()
-let urls = data.map(x => x.git_url)
-
-await $`mkdir -p backups`
-cd('./backups')
-
-await Promise.all(urls.map(url => $`git clone ${url}`))
+  let res = await fetch(`https://api.github.com/users/${username}/repos`, {headers})
+  let data = await res.json()
+  let urls = data.map(x => x.git_url)
+  
+  await $`mkdir -p backups`
+  cd('./backups')
+  
+  await Promise.all(urls.map(url => $`git clone ${url}`))
+})()
